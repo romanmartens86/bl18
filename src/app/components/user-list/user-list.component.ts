4ea8a79363501b5db98fa491fe0879ccf649afbc
Roman <roman.martens@kuka.com>
2018-12-28 12:39:03 +0100

    tests

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,41 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data/data.service';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'bl18-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
+
+
 export class UserListComponent implements OnInit {
 
   errorMessage: string = "StartValue..";
   error: boolean = false;
 
-  users: any[];
-  
-  constructor(private dataServ: DataService) { }
+  //users: any[];
+  users: Array<any> = [];
+  unsortedUsers: any;
 
-  ngOnInit() {
-  }
+  testUsers = [
+    {
+      name: "roman"
+    },
+    {
+      name: "Elvis"
+    },
+    {
+      name: "Moon"
+    },
+    {
+      name: "Moon"
+    },
+    {
+      name: "Sun"
+    }
+  ]
+
+  constructor(private dataServ: DataService) {  }
+
+  ngOnInit() {  }
 
 
-
-
-  
   tryGetUsers(){
     this.dataServ.getUsers().then(res => {
-      this.users = res.val();
+      this.unsortedUsers = res.val();
 
       // log whole object containing all users to console...
-      console.log(this.users);
+      console.log(this.unsortedUsers);
 
-      for (let key of Object.keys(this.users)) {  
-        let User = this.users[key];
+      for (let key of Object.keys(this.unsortedUsers)) {  
+        let User = this.unsortedUsers[key];
 
-        // will only log a single user to console...
-        console.log(User);
+        // to not lose the ID
+        User.ID = key;
+
+        // add user to array
+        this.users.push(User);
       }
 
+      console.log("now the array: ");
+      console.log(this.users);
+
+      this.users = this.users.slice();
 
     }, err => {
       this.errorMessage = err.message;

882e7a4d00516879ec2ddbfd685a6e3f8f9ba154
Roman Martens <rome.martens@googlemail.com>
2019-01-15 10:48:12 +0100

    - u_new list created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,22 +16,33 @@ export const helloFromRoman = functions.https.onRequest((request, response) => {
 
 export const createUserData = functions.auth.user().onCreate((user) =>
 { 	//get firebase user
-	
-	
+
+
+	// if data is empty, fill it with default values
+	if (user.displayName === null) {
+		user.displayName = "Name";
+	}
+	if (user.photoURL === null) {
+		user.photoURL = "url";
+	}
+
 	//  user data administrative list -> for data that should not be seen by everyone
 	admin.database().ref('/u_admin/' + user.uid)
 		.set({ UID: user.uid,
 				email: user.email,
-				level: 10
+				level: 10,
+				u_list: "u_new"
 				});
 	
-	//  user data internal list -> for data that should be open for church members
-	admin.database().ref('/u_intern/' + user.uid)
+	// user data new list -> this list contains all users that registered to bl18,
+	// but are not approved as members
+	admin.database().ref('/u_new/' + user.uid)
     .set({
 		UID: user.uid,
 		name: user.displayName,
       	photoURL: user.photoURL});
   });
+
 
   // on deletion of user -> all saved data has to be removed
   // because of European DataProtection Laws
@@ -45,6 +56,10 @@ export const createUserData = functions.auth.user().onCreate((user) =>
 
 	// user data internal list -> for data that should be open for church members
 	admin.database().ref('/u_intern/' + user.uid)
+		.remove();
+
+	// user data internal list -> for data that should be open for church members
+	admin.database().ref('/u_new/' + user.uid)
 		.remove();
 	
 
